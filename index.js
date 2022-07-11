fa2a4cf55b922bfaffbd0e31a90d21e3dec22b88
SysSimone <developer.desenv@gmail.com>
2022-07-11 15:32:36 -0300

    Rota GET

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const express = require('express') //chamando o módulo  do express e guardando 
 const app = express() // aqui estou inicializando essa constante, inicializando o express
 
 app.listen(3000) /// o listen ouve o navegador e quando eu entrar na porta 3.000 ele joga la pra dentro o que eu quiser.
+
+
+// a barra / representa a rota
+// .GET buscar/pegar
+// .SEND enviar
+app.route('/').get((req,res) =>res.send("hello")) // caminho que o site vai fazer
