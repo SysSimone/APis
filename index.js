4188521c0712a54eb10256b54305723266b306b5
SysSimone <developer.desenv@gmail.com>
2022-07-11 15:13:30 -0300

    Criando projeto com node

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express') //chamando o módulo  do express e guardando nessa constante 
+
+const app = express() // aqui estou inicializando essa constante, inicializando o express
+
+app.listen(3000) /// o listen ouve o navegador e quando eu entrar na porta 3.000 ele joga la pra dentro o que eu quiser.
