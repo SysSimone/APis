400b0ed27951fa369297d597e4cbf298a5bdcd29
SysSimone <developer.desenv@gmail.com>
2022-07-11 21:22:52 -0300

    Delete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express() // aqui estou inicializando essa constante, inicializando 
 app.listen(3000) /// o listen ouve o navegador e quando eu entrar na porta 3.000 ele joga la pra dentro o que eu quiser.
 
 //middleware
-//tudo que entra de informação na api vai passar pelo nosso middleware vai ser transformado em JSON e depois executa.
+//tudo que entra de informação na api vai passar pelo nosso middleware vai ser transformado em JSON e depois executado.
 app.use(express.json()) //metodo dentro do express
 
 // a barra / representa a rota
@@ -17,15 +17,15 @@ app.use(express.json()) //metodo dentro do express
 //app.route('/').get((req,res) =>res.send("hello")) // caminho que o site vai fazer
 // tudo o que é enviado dentro do .Post é recebido dentro do req.
 // o navegador n consegue fazer o .Post, apenas o .GET, vou usar o insomnia
-//app.route('/').post ((req,res) => console.log(req.body))
+app.route('/').post ((req,res) => console.log(req.body))
 
 //quando eu recebo algo na API eu preciso falar para o nosso conteudo que o que estou recebendo é um JSON, para isso vamos usar o:
 //o insomnia vai olhar a rota /, depois procura por um metodo igual ao pedido, antes de executar ele vai pegar o conteudo que ele trouxe da requisição e vai passar pelo middleware, tipo isso
 
-//todo o método acima o resultado da requisição saiu no console do terminal = Console.log, o metodo abaixo na resposta da API sairá o meu conteudo;. 
+//todo o método acima o resultado da requisição saiu no console do terminal = Console.log, o metodo abaixo na resposta da API sairá o meu conteudo: 
 
 //o res.send envia a resposta e o req.body pega o corpo da requisição e envia para o send
-//app.route('/').post ((req,res) => res.send(req.body))
+app.route('/').post ((req,res) => res.send(req.body))
 
 let author = "Simone"
 
@@ -34,4 +34,8 @@ app.route('/').get( (req,res) => res.send(author))
 app.route('/').put( (req,res) => {
     author = req.body.author
     res.send(author)
+})
+//delete
+app.route('/:identificador').delete( (req,res) => {
+    res.send(req.params.identificador)
 })
